6c97e435a414373dd7f3de22c4944ce53f723820
jakub siluszyk <jakub.siluszyk@gmail.com>
2023-08-18 14:30:27 +0200

    Added doxystrings

--- src/isr.hpp
+++ src/isr.hpp
@@ -4,6 +4,7 @@
 extern volatile bool limitSwitch1Touched;
 extern volatile bool limitSwitch2Touched;
 
+/// @brief Include Stopping motor - interrupt
 void limitSwitch1ISR();
 void limitSwitch2ISR();
 
